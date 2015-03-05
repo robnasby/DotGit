2e1513da79ab61a6832108349753eb86835ad3fb
Rob Nasby <rob.nasby@mcmaster.com>
2015-03-05 16:36:07 -0600

    Add methods to amend the head commit.

--- source/DotGit/Operations/CommitOperations.vb
+++ source/DotGit/Operations/CommitOperations.vb
@@ -20,6 +20,28 @@ Public Class CommitOperations
 
 #Region "Methods"
 
+    Public Sub Amend()
+
+        Dim arguments As String = "--amend --no-edit"
+        Dim command As New Command("commit", commandArguments:=arguments, repositoryPath:=Repository.Path)
+        command.Execute()
+
+        If Not command.Status = CommandStatusOption.SUCCEEDED Then _
+            Throw New ApplicationException("Unable to amend the commit.")
+
+    End Sub
+
+    Public Sub Amend(message As String)
+
+        Dim arguments As String = String.Format("--amend -m ""{0}""", message)
+        Dim command As New Command("commit", commandArguments:=arguments, repositoryPath:=Repository.Path)
+        command.Execute()
+
+        If Not command.Status = CommandStatusOption.SUCCEEDED Then _
+            Throw New ApplicationException("Unable to amend the commit.")
+
+    End Sub
+
     Public Sub Create(message As String)
 
         Dim arguments As String = String.Format("-m ""{0}""", message)
--- tests/DotGitTests/Operations/CommitOperationsTests.vb
+++ tests/DotGitTests/Operations/CommitOperationsTests.vb
@@ -4,6 +4,33 @@ Imports Xunit
 Public Class CommitOperationsTests
 
     <Fact>
+    Public Sub AmendCommitContentsSucceeds()
+
+        TestHelper.OnPersonalRepository(
+            Sub(repo As Repository)
+                repo.Index.Add(TestHelper.CreateFileInDirectory(repo.Path))
+                repo.Commit.Create("Test commit")
+
+                repo.Index.Add(TestHelper.CreateFileInDirectory(repo.Path))
+                repo.Commit.Amend()
+            End Sub)
+
+    End Sub
+
+    <Fact>
+    Public Sub AmendCommitMessageSucceeds()
+
+        TestHelper.OnPersonalRepository(
+            Sub(repo As Repository)
+                repo.Index.Add(TestHelper.CreateFileInDirectory(repo.Path))
+                repo.Commit.Create("Test commit")
+
+                repo.Commit.Amend("Better message")
+            End Sub)
+
+    End Sub
+
+    <Fact>
     Public Sub CreateNewCommitSucceeds()
 
         TestHelper.OnPersonalRepository(
