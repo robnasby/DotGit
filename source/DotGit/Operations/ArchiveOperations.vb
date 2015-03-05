48a9f80aed4a760b8cc2281f8d6471894e2ad28e
Rob Nasby <rob.nasby@mcmaster.com>
2015-03-05 16:56:30 -0600

    Add methods to retrieve an archive of a commit within a repository.

--- source/DotGit/Operations/ArchiveOperations.vb
+++ source/DotGit/Operations/ArchiveOperations.vb
@@ -1,4 +1,15 @@
-﻿Public Class ArchiveOperations
+﻿Imports DotGit.Internal
+
+Public Class ArchiveOperations
+
+#Region "Constants"
+
+    Public Enum FormatOption
+        TAR
+        ZIP
+    End Enum
+
+#End Region
 
 #Region "Properties"
 
@@ -11,6 +22,23 @@
     Friend Sub New(repository As Repository)
 
         Me.Repository = repository
+
+    End Sub
+
+#End Region
+
+#Region "Methods"
+
+    Public Sub [Get](committish As String,
+                     outputFilePath As String,
+                     format As ArchiveOperations.FormatOption)
+
+        Dim arguments As String = String.Format("-o ""{0}"" --format={1} {2}", outputFilePath, format.ToString.ToLower, committish)
+        Dim command As New Command("archive", commandArguments:=arguments, repositoryPath:=Me.Repository.Path)
+        command.Execute()
+
+        If Not command.Status = CommandStatusOption.SUCCEEDED Then _
+            Throw New ApplicationException(String.Format("Unable to save archive for '{0}' to '{1}.'", committish, outputFilePath))
 
     End Sub
 
--- tests/DotGitTests/Operations/ArchiveOperationsTests.vb
+++ tests/DotGitTests/Operations/ArchiveOperationsTests.vb
@@ -1,6 +1,25 @@
 ﻿Imports DotGit
+Imports System.IO
 Imports Xunit
 
 Public Class ArchiveOperationsTests
+
+    <Fact>
+    Public Sub ArchivingPersonalPreositorySucceeds()
+
+        TestHelper.OnPersonalRepository(
+            Sub(repo As Repository)
+                repo.Index.Add(TestHelper.CreateFileInDirectory(repo.Path))
+                repo.Commit.Create("Test commit")
+
+                TestHelper.OnTempDirectory(
+                    Sub(archiveDirectoryPath As String)
+                        Dim archiveFilePath As String = Path.Combine(archiveDirectoryPath, "archive.zip")
+                        repo.Archive.Get("HEAD", archiveFilePath, ArchiveOperations.FormatOption.ZIP)
+                        Assert.True(File.Exists(archiveFilePath))
+                    End Sub)
+            End Sub)
+
+    End Sub
 
 End Class
