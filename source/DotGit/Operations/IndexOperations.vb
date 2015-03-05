a727aeda9ee79c3a9cbc81083220576168d4faff
Rob Nasby <rob.nasby@mcmaster.com>
2015-03-05 15:01:17 -0600

    Add methods to remove files from the index.

--- source/DotGit/Operations/IndexOperations.vb
+++ source/DotGit/Operations/IndexOperations.vb
@@ -59,6 +59,45 @@ Public Class IndexOperations
 
     End Sub
 
+    ''' <summary>
+    ''' Remove a file from the index of a repository.
+    ''' </summary>
+    ''' <param name="path">
+    ''' The path to the file in the repository.
+    ''' </param>
+    ''' <param name="force">
+    ''' Force remove the file, even if it could result in lost data.
+    ''' </param>
+    Public Sub Remove(path As String,
+             Optional force As Boolean = False)
+
+        Remove({path}, force)
+
+    End Sub
+
+    ''' <summary>
+    ''' Remove a file from the index of a repository.
+    ''' </summary>
+    ''' <param name="paths">
+    ''' The paths to the files in the repository.
+    ''' </param>
+    ''' <param name="force">
+    ''' Force remove the file, even if it could result in lost data.
+    ''' </param>
+    Public Sub Remove(paths As IEnumerable(Of String),
+               Optional force As Boolean = False)
+
+        Dim arguments As String = String.Format("--cached -- {0}", String.Join(" ", paths.Select(Function(p As String) String.Format("""{0}""", p))))
+        If force Then arguments = "-f " + arguments
+        Dim command As New Command("rm", commandArguments:=arguments, repositoryPath:=Me.Repository.Path)
+        command.Execute()
+
+        If Not command.Status = CommandStatusOption.SUCCEEDED Then _
+            Throw New ApplicationException("Unable to remove files from the index." + Environment.NewLine +
+                                           String.Join(Environment.NewLine, paths.Select(Function(p As String) String.Format("'{0}'", p))))
+
+    End Sub
+
 #End Region
 
 End Class
--- tests/DotGitTests/Operations/IndexOperationsTests.vb
+++ tests/DotGitTests/Operations/IndexOperationsTests.vb
@@ -25,4 +25,28 @@ Public Class IndexOperationsTests
 
     End Sub
 
+    <Fact>
+    Public Sub RemovingFileFromIndexSucceeds()
+
+        TestHelper.OnPersonalRepository(
+           Sub(repo As Repository)
+               Dim filePath As String = TestHelper.CreateFileInDirectory(repo.Path)
+               repo.Index.Add(filePath)
+               repo.Index.Remove(filePath)
+           End Sub)
+
+    End Sub
+
+    <Fact>
+    Public Sub RemovingFilesFromIndexSucceeds()
+
+        TestHelper.OnPersonalRepository(
+           Sub(repo As Repository)
+               Dim filePaths As IEnumerable(Of String) = TestHelper.CreateFilesInDirectory(repo.Path, 10)
+               repo.Index.Add(filePaths)
+               repo.Index.Remove(filePaths)
+           End Sub)
+
+    End Sub
+
 End Class
