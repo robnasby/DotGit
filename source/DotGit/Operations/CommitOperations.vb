4f75c172a2eba66efddac97b963027f264622d6f
Rob Nasby <rob.nasby@mcmaster.com>
2015-03-05 16:06:33 -0600

    Add a method to create a new commit.

--- source/DotGit/Operations/CommitOperations.vb
+++ source/DotGit/Operations/CommitOperations.vb
@@ -1,4 +1,6 @@
-﻿Public Class CommitOperations
+﻿Imports DotGit.Internal
+
+Public Class CommitOperations
 
 #Region "Properties"
 
@@ -11,6 +13,21 @@
     Friend Sub New(repository As Repository)
 
         Me.Repository = repository
+
+    End Sub
+
+#End Region
+
+#Region "Methods"
+
+    Public Sub Create(message As String)
+
+        Dim arguments As String = String.Format("-m ""{0}""", message)
+        Dim command As New Command("commit", commandArguments:=arguments, repositoryPath:=Repository.Path)
+        command.Execute()
+
+        If Not command.Status = CommandStatusOption.SUCCEEDED Then _
+            Throw New ApplicationException("Unable to create a commit.")
 
     End Sub
 
--- tests/DotGitTests/Operations/CommitOperationsTests.vb
+++ tests/DotGitTests/Operations/CommitOperationsTests.vb
@@ -3,4 +3,15 @@ Imports Xunit
 
 Public Class CommitOperationsTests
 
+    <Fact>
+    Public Sub CreateNewCommitSucceeds()
+
+        TestHelper.OnPersonalRepository(
+            Sub(repo As Repository)
+                repo.Index.Add(TestHelper.CreateFileInDirectory(repo.Path))
+                repo.Commit.Create("Test commit")
+            End Sub)
+
+    End Sub
+
 End Class
